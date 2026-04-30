006a5b52095f24142c74867aa32da2edfdfeded0
sanda9203 <sanda9203@gmail.com>
2026-04-30 17:39:45 +0900

    first commit

---
+++ ex01.py
@@ -0,0 +1,12 @@
+
+food = ["김밥", "만두", "양념치킨", "족발", "피자", "쫄면", "라면"]
+
+
+it_food = iter(food)
+print(type(it_food))
+
+for food in it_food:
+    print(food)
+
+print(next(it_food))    #STtopIteration
+
